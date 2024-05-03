a2a70b987bf09a75e0f34dfac9bba72b74b019d9
Nuel Elekwachi <elekwaeke300@gmail.com>
2024-05-03 18:14:39 +0100

    added metadata

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
@@ -5,6 +6,10 @@ import { getFeaturedPosts } from "../lib/posts-util";
 function HomePage(props) {
   return (
     <>
+      <Head>
+        <title>Max Blog</title>
+        <meta name="description" content="I post about web development" />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
@@ -22,36 +27,3 @@ export function getStaticProps() {
 }
 
 export default HomePage;
-/*
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ijkmnffs:Dem0cracy@blog.0zcapwl.mongodb.net/?retryWrites=true&w=majority&appName=blog";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-*/
-
-/*
-mongodb+srv://ijkmnffs:<password>@blog.0zcapwl.mongodb.net/
-*/
